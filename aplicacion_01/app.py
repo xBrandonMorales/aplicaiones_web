b82c02ffbd5bfcb2a79befd8dbd52e811f7c143e
Brandon Castro Morales <adrimora.0232@gmail.com>
2024-02-06 18:52:43 +0000

    ggg

--- aplicacion_01/app.py
+++ aplicacion_01/app.py
@@ -3,12 +3,13 @@ import web
 urls = (
     "/", "hello",
     "/pagina2", "pagina2"
+    "/",
 )
 app = web.application(urls, globals())
 
 class hello:
     def GET(self):
-        return 'Hola pagina 1!'
+        return 'Hola estas en la super mega pagina 1!'
 
 class pagina2:
     def GET(self):
--- aplicacion_02/app.py
+++ aplicacion_02/app.py
@@ -2,15 +2,25 @@ import web
 
 # Rutas de los controladores
 urls = (
-    '/(.*)', 'mvc.controllers.hello.Hello'
+    "/", "hello",
+    "/pagina1", "pagina1",
+    "/pagina2", "pagina2"  
 )
 app = web.application(urls, globals())
 
 class hello:
-    def GET(self, name):
+    def GET(self, name=None):
         if not name:
-            name = 'World_2'
+            name = 'World'
         return 'Hola, ' + name + '!'
+
+class pagina1:
+    def GET(self):
+        return "PAGINA 1"
+
+class pagina2:  
+    def GET(self):
+        return "Hola, estás en la página 2. ¡Sientes mucha emoción!"
 
 # Punto de entrada
 if __name__ == "__main__":
